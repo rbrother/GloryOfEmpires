8c3cc2fd7d2f0128fbd9b3ca73613b7d9d0af614
Robert Brotherus <robert@iki.fi>
2015-03-13 20:40:49 +0200

    Upgrade to Unity5

--- Assets/Scripts/CameraPanning.cs
+++ Assets/Scripts/CameraPanning.cs
@@ -9,7 +9,7 @@ public class CameraPanning : MonoBehaviour {
 	private const float MIN_CAMERA_SIZE = 2.0f;
 
 	void Start () {
-		targetCameraSize = this.camera.orthographicSize;
+		targetCameraSize = this.GetComponent<Camera>().orthographicSize;
 	}
 	
 	// Update is called once per frame
@@ -19,15 +19,15 @@ public class CameraPanning : MonoBehaviour {
 		if (mouseWheel != 0) this.targetCameraSize *= (1.0f - mouseWheel * 1.0f);
 		if (this.targetCameraSize > MAX_CAMERA_SIZE) this.targetCameraSize = MAX_CAMERA_SIZE;
 		if (this.targetCameraSize < MIN_CAMERA_SIZE) this.targetCameraSize = MIN_CAMERA_SIZE;
-		var deltaSize = (targetCameraSize - camera.orthographicSize);
-		camera.orthographicSize += deltaSize * Time.deltaTime * 5.0f;
+		var deltaSize = (targetCameraSize - GetComponent<Camera>().orthographicSize);
+		GetComponent<Camera>().orthographicSize += deltaSize * Time.deltaTime * 5.0f;
 		// Panning with mouse button
 		if (Input.GetMouseButtonDown (1)) {
 			lastPosition = Input.mousePosition;
 		}
 		if (Input.GetMouseButton (1)) {
 			var deltaPos = ( lastPosition - Input.mousePosition );
-			var deltaCamera = deltaPos * camera.orthographicSize * 2.0f / Screen.height;
+			var deltaCamera = deltaPos * GetComponent<Camera>().orthographicSize * 2.0f / Screen.height;
 			transform.Translate( deltaCamera.x, deltaCamera.y, 0 );
 			lastPosition = Input.mousePosition;
 		}
